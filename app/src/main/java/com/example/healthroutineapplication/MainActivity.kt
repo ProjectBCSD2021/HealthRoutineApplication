86bbdbd3a7b1d8a372e3f4f944ee8c6f4e6c9bf4
HanSungHyun <han990102@naver.com>
2021-08-14 21:51:52 +0900

    Make MainActivity
    메인엑티비티 기본 틀

--- app/src/main/java/com/example/healthroutineapplication/MainActivity.kt
+++ app/src/main/java/com/example/healthroutineapplication/MainActivity.kt
@@ -1,11 +1,65 @@
 package com.example.healthroutineapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.ListFragment
+import com.example.healthroutineapplication.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    private val mOnNavigationItemSelectedListener =
+        BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.list -> {
+                    replaceFragment(MainFragment())
+                    return@OnNavigationItemSelectedListener true
+                }
+
+                R.id.calendar -> {
+                    replaceFragment(CalendarFragment())
+                    return@OnNavigationItemSelectedListener true
+                }
+            }
+            false
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val toolbar: Toolbar = binding.toolbar
+        setSupportActionBar(toolbar)
+        val actionBar: ActionBar = supportActionBar!!
+        actionBar.setDisplayShowTitleEnabled(false)
+        replaceFragment(MainFragment())
+
+        binding.bottomNaviBar.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+        binding.shoppingButton.setOnClickListener {
+            Toast.makeText(this, "추가하기", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.mainLayout, fragment)
+        fragmentTransaction.commit()
     }
 }
+
+
